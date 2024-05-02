fe9794d467efa0b423bec1e2b292403de77a5dcc
Peter Kranz <peter@myref.net>
2024-05-02 22:07:27 +0200

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,46 @@
+/* eslint no-unused-vars: off */
+/* eslint no-global-assign: off */
+/* global systemDictionary */
+"use strict";
+
+systemDictionary = {
+	"pichlerconnect adapter settings": {
+		en: "Adapter settings for pichlerconnect",
+		de: "Adaptereinstellungen für pichlerconnect",
+		ru: "Настройки адаптера для pichlerconnect",
+		pt: "Configurações do adaptador para pichlerconnect",
+		nl: "Adapterinstellingen voor pichlerconnect",
+		fr: "Paramètres d'adaptateur pour pichlerconnect",
+		it: "Impostazioni dell'adattatore per pichlerconnect",
+		es: "Ajustes del adaptador para pichlerconnect",
+		pl: "Ustawienia adaptera dla pichlerconnect",
+		uk: "Налаштування адаптера для pichlerconnect",
+		"zh-cn": "pichlerconnect的适配器设置",
+	},
+	option1: {
+		en: "option1",
+		de: "Option 1",
+		ru: "Опция 1",
+		pt: "Opção 1",
+		nl: "Optie 1",
+		fr: "Option 1",
+		it: "opzione 1",
+		es: "Opción 1",
+		pl: "opcja 1",
+		uk: "варіант1",
+		"zh-cn": "选项1",
+	},
+	option2: {
+		en: "option2",
+		de: "Option 2",
+		ru: "вариант 2",
+		pt: "opção 2",
+		nl: "Optie 2",
+		fr: "Option 2",
+		it: "opzione 2",
+		es: "opcion 2",
+		pl: "Opcja 2",
+		uk: "варіант2",
+		"zh-cn": "选项2",
+	},
+};
